ea50b67ee900380fe39e1f6bbb4586b1645ac7ff
pupasaki <pupasaki@x241.ca>
2017-07-06 19:20:07 -0400

    Added downvoting, changed color for voted vs non voted

--- imports/ui/components/VoteControl.js
+++ imports/ui/components/VoteControl.js
@@ -8,37 +8,45 @@ export default class VoteControl extends Component {
     super(props);
     this.state = {}
     this.vote = this.vote.bind(this)
+    this.unvote = this.unvote.bind(this)
   }
 
   vote() {
     if (!Meteor.user()) {
       // please login before voting
-      console.log("pls login")
+      console.log("please login")
       return
     }
 
-    console.log('vote()')
-    console.log(this.props.votes)
-
     if (!this.props.votes || this.props.votes.length < 1) {
-      console.log("voted post id: " + this.props.post._id)
 
-      // TODO: try to ensure only one is created
+      // TODO: update this to an atomic counter
       Votes.insert({
         userId: this.props.user._id,
         postId: this.props.post._id,
         createdAt: new Date(),
-      })
+      }, () => { Posts.update(this.props.post._id, { $inc: { score: 1 }}) })
 
-      Posts.update(this.props.post._id,
-                  { $inc: { score: 1 }})
+
+    } else if (this.props.votes.length > 0) {
+      // clicking should remove vote
+
     }
   }
 
+  unvote() {
+      // TODO: update this to an atomic counter
+      Votes.remove({ _id: this.props.votes[0]._id },
+                   () => { Posts.update(this.props.post._id, { $inc: { score: -1 }}) })
+
+  }
+
   render() {
+    const voted = (this.props.votes.length > 0)
+
     return (
       <div className="voteControl">
-        <Icon link name='chevron up' onClick={this.vote} />
+        <Icon link name='chevron up' color={voted ? 'orange' : 'black'} onClick={voted ? this.unvote : this.vote} />
         { this.props.post.score }
       </div>
     )
--- imports/ui/components/VoteControlContainer.js
+++ imports/ui/components/VoteControlContainer.js
@@ -9,9 +9,7 @@ import VoteControl from './VoteControl.js';
 
 export default VoteControlContainer = createContainer(({ post, user }) => {
   var votes
-  console.log(Meteor.user())
   if (user) {
-    console.log("here")
     const votesHandle = Meteor.subscribe('votes', {userId: user._id, postId: post._id})
     votes = Votes.find({postId: post._id, userId: user._id}).fetch()
   }
